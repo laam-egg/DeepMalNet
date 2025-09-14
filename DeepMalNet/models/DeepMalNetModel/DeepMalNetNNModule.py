fix: apply fixed scaling

DeepMalNet/models/DeepMalNetModel/DeepMalNetNNModule.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from .hyperparams_loader import ScalingHyperparams
 
 class DeepMalNet_Mode:
     TRAINING = "training"
@@ -44,12 +45,14 @@ def deepmalnet_hidden_layers(input_dim, units, dropout, mode):
 
 class DeepMalNetNNModule(nn.Module):
     def __init__(self, input_dim, mode, dropout=0.01):
+        # type: (DeepMalNetNNModule, int, str, float) -> None
+
         print(f"Establishing DeepMalNetNNModule ---")
         print(f"    - mode      =   {mode}")
         print(f"    - dropout   =   {dropout}")
         print(f"---")
         super(DeepMalNetNNModule, self).__init__()
-        
+
         layers = []
 
         hidden_dim_changes = [4608, 4096, 3584, 3072, 2560, 2048, 1536, 1024, 512, 128]
@@ -73,5 +76,38 @@ class DeepMalNetNNModule(nn.Module):
         # Pack into Sequential
         self.model = nn.Sequential(*layers)
 
+        # Temporary default
+        scaling_hyperparams = ScalingHyperparams.load_default()
+        mean = scaling_hyperparams.mean
+        std = scaling_hyperparams.std
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
+
     def forward(self, x):
+        x = (x - self.mean) / self.std
         return self.model(x)
+    
+    def load_model_data(self, model_data):
+        # type: (DeepMalNetNNModule, dict) -> None
+        state_dict = model_data["model_state_dict"]
+        try:
+            mean = state_dict["mean"]
+            std = state_dict["std"]
+        except KeyError as e:
+            print(f"[WARN] Model doesn't have stored mean/std scaling hyperparams ; using defaults")
+            scaling_hyperparams = ScalingHyperparams.load_default()
+            mean = scaling_hyperparams.mean
+            std = scaling_hyperparams.std
+        else:
+            print(f"[INFO] Loading model's scaling hyperparams...")
+
+        self.load_state_dict(state_dict)
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
+    
+    def get_scaling_hyperparams(self):
+        # type: (DeepMalNetNNModule) -> ScalingHyperparams
+        return ScalingHyperparams(
+            mean=self.mean,
+            std=self.std,
+        )

DeepMalNet/models/DeepMalNetModel/InferenceDeepMalNetModel.py
@@ -33,13 +33,8 @@ class InferenceDeepMalNetModel(AbstractModel):
                 model_data = torch.load(model_path, map_location=torch.device('cpu'))
             else:
                 raise
-        
-        try:
-            state_dict = model_data["model_state_dict"]
-        except KeyError:
-            state_dict = model_data
 
-        self._model.load_state_dict(state_dict)
+        self._model.load_model_data(model_data)
         self._model.eval()
 
         DEVICE_NAME = "cuda:0"

DeepMalNet/training/__init__.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 from tqdm import tqdm
 
 from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 import os
 import re
@@ -67,9 +68,11 @@ class Trainer:
         print(f"[INFO] Loading checkpoint: {path}")
 
         checkpoint = torch.load(path, map_location=self.DEVICE_NAME)
-        self.model.load_state_dict(checkpoint["model_state_dict"])
+        self.model.load_model_data(checkpoint)
         if "optimizer_state_dict" in checkpoint:
             self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        if "scheduler_state_dict" in checkpoint:
+            self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
         self.last_epoch = checkpoint["epoch"]
         self.last_loss = checkpoint.get("loss", float("nan"))
 
@@ -115,6 +118,8 @@ class Trainer:
             avg_loss = total_loss / len(train_loader)
 
             self.last_loss = avg_loss
+
+            self.scheduler.step()
 
             # Validation
 
@@ -166,6 +171,7 @@ class Trainer:
             {
                 "model_state_dict": self.model.state_dict(),
                 "optimizer_state_dict": self.optimizer.state_dict(),
+                "scheduler_state_dict": self.scheduler.state_dict(),
                 "epoch": self.last_epoch,
                 "loss": self.last_loss,
             },
@@ -194,6 +200,11 @@ class Trainer:
             self.model.parameters(),
             lr=2e-4,
             weight_decay=1e-2,
+        )
+        self.scheduler = CosineAnnealingLR(
+            self.optimizer,
+            T_max=self.NUM_EPOCHS_TO_RUN_THIS_TIME,
+            eta_min=1e-6, # minimum learning rate
         )
     
     def _transfer_model_to_accelerator(self):